72821a398f4939af26dad05e4291df4bcea1821f
Eli <ebweinberger>
2021-02-28 01:48:19 -0500

    price

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Price from './components/price'
 
 function App() {
   return (
     <div>
       <header>
         Start developing here!
+        <Price></Price>
       </header>
     </div>
   );
---
+++ src/components/price.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Price = () => {
+    return(
+        <p>Price here</p>
+    )
+}
+
+export default Price
